fd53cbd94f9281cdb9ab55c0256a994bfd04a14f
Kalynovsky Valentin <nakama3942@gmail.com>
2022-11-07 14:34:31 +0200

    Script documented and README.md added

---
+++ README.md
@@ -0,0 +1,49 @@
+# NameMail
+The program is a solution to the task from the fifth laboratory on the subject "Network Information Technologies".
+
+## Overview
+This program is a mail client. Its main functionality:
+- Obtaining the email address to which the phishing attack will be carried out;
+- Getting a phishing link that will be installed in a phishing email;
+- Formation and sending of a persuasive letter;
+- All background information and documentation is described in script files;
+- And a lot of other important little things.
+
+## LICENSE
+The full text of the license can be found at the following [link](https://github.com/Nakama3942/Phishing/blob/main/LICENSE).
+
+> Copyright © 2022 Kalynovsky Valentin. All rights reserved.
+> 
+> Licensed under the Apache License, Version 2.0 (the "License");
+> you may not use this file except in compliance with the License.
+> You may obtain a copy of the License at
+> 
+>     http://www.apache.org/licenses/LICENSE-2.0
+> 
+> Unless required by applicable law or agreed to in writing, software
+> distributed under the License is distributed on an "AS IS" BASIS,
+> WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+> See the License for the specific language governing permissions and
+> limitations under the License.
+
+## Usage
+To start the program, just run *main.py*.
+
+But it is also possible to run the program through the console:
+```shell
+python main.py
+```
+
+## Authors
+<table align="center" style="border-width: 10; border-style: ridge">
+	<tr>
+		<td align="center"><a href="https://github.com/Nakama3942"><img src="https://avatars.githubusercontent.com/u/73797846?s=400&u=a9b7688ac521d739825d7003a5bd599aab74cb76&v=4" width="150px;" alt=""/><br /><sub><b>Kalynovsky Valentin</b></sub></a><sub><br />"Ideological inspirer and Author"</sub></td>
+		<!--<td></td>-->
+	</tr>
+<!--
+	<tr>
+		<td></td>
+		<td></td>
+	</tr>
+-->
+</table>
--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+#  Copyright © 2022 Kalynovsky Valentin. All rights reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 import configparser
 import smtplib
 import datetime
@@ -6,6 +20,10 @@ from email.mime.text import MIMEText
 
 
 if __name__ == '__main__':
+    # Reading the login and password from which phishing will
+    # be sent. If the data file is missing, then the program
+    # is launched on the computer for the first time and the
+    # authorization stage begins with saving the data.
     email_sender = ""
     email_password = ""
     try:
@@ -33,6 +51,7 @@ if __name__ == '__main__':
         with open('config.ini', 'w') as config_file:
             config.write(config_file)
 
+    # Preparing the text of the letter for sending
     email_receiver = input("\nEnter the recipient's address: ")
     phish_link = input("\nEnter the phishing link: ")
     with open("message.txt", "r", encoding="utf-8") as msg:
@@ -40,17 +59,17 @@ if __name__ == '__main__':
         subject, body = read_message.split("\n$separator$\n")
         body = body.replace("$date", str(datetime.datetime.today()).split(" ")[0])
         body = body.replace("$link", phish_link)
-
     print("Wait, the letter is being sent")
 
+    # Formation of a letter package for sending
     message = MIMEMultipart()
     message['From'] = email_sender
     message['To'] = email_receiver
     message['Subject'] = subject
     message.attach(MIMEText(body, 'html'))
 
+    # Sending letter
     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     server.login(email_sender, email_password)
     server.sendmail(email_sender, email_receiver, message.as_string())
-
     print("The letter was sent successfully")
